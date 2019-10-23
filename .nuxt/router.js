4b6b2883e82c8fcb83efa2e1d51cf3c2867f40ee
Vladick Afanasjew <afanasjew.vladick@yandex.ru>
2019-10-23 22:22:59 +0300

    Добавлена навигация по страницам

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -3,6 +3,9 @@ import Router from 'vue-router'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
+const _6dc9fe0e = () => interopDefault(import('../pages/Multiplayer.vue' /* webpackChunkName: "pages/Multiplayer" */))
+const _5e142a44 = () => interopDefault(import('../pages/Single.vue' /* webpackChunkName: "pages/Single" */))
+const _6863c0c2 = () => interopDefault(import('../pages/Statistics.vue' /* webpackChunkName: "pages/Statistics" */))
 const _0f502ea6 = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
 
 Vue.use(Router)
@@ -15,6 +18,18 @@ export const routerOptions = {
   scrollBehavior,
 
   routes: [{
+    path: "/Multiplayer",
+    component: _6dc9fe0e,
+    name: "Multiplayer"
+  }, {
+    path: "/Single",
+    component: _5e142a44,
+    name: "Single"
+  }, {
+    path: "/Statistics",
+    component: _6863c0c2,
+    name: "Statistics"
+  }, {
     path: "/",
     component: _0f502ea6,
     name: "index"
---
+++ pages/Statistics.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="message">
+    <h1>Раздел в разработке!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss">
+.message {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  min-height: 760px;
+}
+</style>
